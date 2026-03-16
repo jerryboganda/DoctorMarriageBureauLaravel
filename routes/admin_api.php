63f6504947ecef5ed5f4899c12a539c96eb9b216
jerryboganda <jerryboganda@example.com>
2026-03-17 01:03:39 +0500

    fix: resolve route name conflict by adding admin-api prefix to admin_api routes

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Api\Admin'], function () {
+Route::group(['prefix' => 'admin', 'as' => 'admin-api.', 'namespace' => 'Api\Admin'], function () {
     Route::post('/login', 'AuthController@login');
 
     Route::group(['middleware' => ['auth:sanctum', 'api_admin']], function () {
