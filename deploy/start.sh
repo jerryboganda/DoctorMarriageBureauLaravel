1cd63813ab38bc43b5daae9a992f7c3dfb9d629b
Dr Faisal Maqsood <dr-faisal-maqsood@users.noreply.github.com>
2026-06-05 17:54:45 +0500

    Commit preserved local startup fixes

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/bin/sh
+
 # Install cron job for Laravel scheduler
-echo '* * * * * cd /var/www && php artisan schedule:run >> /dev/null 2>&1' | crontab -
+echo '* * * * * cd /var/www && /usr/local/bin/php artisan schedule:run >> /dev/null 2>&1' | crontab -
 service cron start
 
 # Run composer install and start php-fpm
+mkdir -p /var/www/bootstrap/cache
 composer install --no-interaction --optimize-autoloader --no-dev
+
+if [ ! -e /var/www/public/storage ]; then
+  php artisan storage:link
+fi
+
 php artisan config:cache || true
 php artisan event:cache || true
 php artisan view:cache || true
